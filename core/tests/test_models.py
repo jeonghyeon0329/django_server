9d2db0dc8f28d1c8d08e8d1eece253dfcd6442ab
jeonghyeon <sptlsrja@gmail.com>
2025-09-13 18:00:00 +0900

    feature : pytest status_code test 100~599

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,28 +1,28 @@
-# import pytest
-# from django.db import IntegrityError
-# from django.db.models import ProtectedError
+import pytest
+from django.db import IntegrityError
+from django.db.models import ProtectedError
 
-# @pytest.mark.django_db
-# def test_idempotency_status_code_constraint_allows_valid():
-#     from core.models import IdempotencyKey
-#     obj = IdempotencyKey.objects.create(
-#         key="k-ok",
-#         request_hash="h1",
-#         status_code=200,
-#         response_body="{}",
-#     )
-#     assert obj.pk is not None
+@pytest.mark.django_db
+def test_idempotency_status_code_constraint_allows_valid():
+    from core.models import IdempotencyKey
+    obj = IdempotencyKey.objects.create(
+        key="k-ok",
+        request_hash="h1",
+        status_code=200,
+        response_body="{}",
+    )
+    assert obj.pk is not None
 
-# @pytest.mark.django_db
-# def test_idempotency_status_code_constraint_blocks_invalid():
-#     from core.models import IdempotencyKey
-#     with pytest.raises(IntegrityError):
-#         IdempotencyKey.objects.create(
-#             key="k-bad",
-#             request_hash="h1",
-#             status_code=42,  # 0 또는 100~599만 허용
-#             response_body="{}",
-#         )
+@pytest.mark.django_db
+def test_idempotency_status_code_constraint_blocks_invalid():
+    from core.models import IdempotencyKey
+    with pytest.raises(IntegrityError):
+        obj = IdempotencyKey.objects.create(
+            key="k-bad",
+            request_hash="h1",
+            status_code=600,  # 0 또는 100~599만 허용(그외는 비정상 상태코드)
+            response_body="{}",
+        )
 
 # @pytest.mark.django_db
 # def test_outbox_defaults_and_protect(tenant):
